5fdc0a5c45c5eea5c859af5847e395ab953fab68
Dmitry <df.creative@gmail.com>
2016-08-24 20:44:33 -0400

    Handle extensionless dotfiles

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = extname;
 function extname (str) {
 	let slug = str.split(/\/|\\/).slice(-1)[0];
 	let idx = slug.lastIndexOf('.');
-	if (idx < 0) return '';
+	if (idx <= 0) return '';
 	let ext = slug.slice(idx);
 	return ext;
 }
